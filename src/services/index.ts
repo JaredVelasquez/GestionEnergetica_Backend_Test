1223c3121aa624745c063d7181ca1f4109606be8
Jared Velasquez <jared.velasquez1122@gmail.com>
2022-04-08 14:06:59 -0600

    Arreglando vulnerabilidades

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,13 +1,12 @@
+export * from './actor.service';
 export * from './Auth.service';
 export * from './encript-decrypt.service';
 export * from './estrategy.service';
+export * from './file-upload.service';
+export * from './image.service';
+export * from './invoices.service';
 export * from './jwt.service';
+export * from './meter.service';
 export * from './notify.service';
 export * from './user.service';
 
-
-export * from './image.service';
-export * from './actor.service';
-export * from './invoices.service';
-export * from './file-upload.service';
-export * from './meter.service';
